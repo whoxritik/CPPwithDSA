1430b6bd6c6e54264c0c8c503c4c82c459534a67
whoxritik <ritik.singh_cs23@gla.ac.in>
2026-07-12 02:31:02 +0530

    commit

---
+++ .GFG/Alternates in an Array.cpp
@@ -0,0 +1,11 @@
+// class Solution {
+//   public:
+//     vector<int> getAlternates(vector<int> &arr) {
+//         // code here
+//         vector<int> ans;
+//         for(int i=0; i<arr.size(); i=i+2){
+//             ans.push_back(arr[i]);
+//         }
+//         return ans;
+//     }
+// };
